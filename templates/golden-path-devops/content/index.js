2bc1de29d7a9e02a502a0c125578851462f38461
DxP POC <dxp@poc.local>
2026-06-01 19:16:05 +0000

    fix: port 3000 + namespace name-dev dans Golden Path

--- templates/golden-path-devops/content/index.js
+++ templates/golden-path-devops/content/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
